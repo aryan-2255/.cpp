d7932e6c5b2444a48fe9cfd8b9ba6fa56d48b2df
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-08-27 06:25:14 +0530

    Add C++ beginner programs

---
+++ helloworld.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void first(); 
+
+int main(){
+    std :: cout << "i am aryan choudhary" << std :: endl;
+    std :: cout << "i am aryan" << '\n';
+    first();
+    return 0;
+}   
+
+void first(){
+    std :: cout << "i am aryan choudhary" << '\n';
+    std :: cout << "i am ";
+    
+}   
+
+
+// comment
+
+/* 
+multi line comment
+*/
+
+
