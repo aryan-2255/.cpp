4fbebb529099ac4349eef66fe32d125021a0921a
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-08-27 06:58:14 +0530

    basics

---
+++ 03_datatype.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(){
+   
+   // integer (Whole Number) no decimal
+   int age = 21;
+   int year = 2023;
+   int days = 7;
+
+   // double  (number including decimal)
+   double price = 10.99;
+   double gpa = 2.5;
+   double temperature = 25.1;
+
+   // single character (only store single character)
+   char grade = 'A';
+   char initial = 'B';
+   char currency = '$';
+
+   // boolean (true or false)
+   bool student = false;
+   bool power = true;
+
+
+
+
+   std::cout << initial;
+   // cout<<(grade) in tubro c++
+
+
+    return 0;
+}   
