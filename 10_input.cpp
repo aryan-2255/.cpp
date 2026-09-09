6c39a5a99b7694d0060308b4a8b9fcb08411cd21
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-09-10 04:07:39 +0530

    adD

--- 10_input.cpp
+++ 10_input.cpp
@@ -8,16 +8,16 @@ int main(){
     std::string name;
     int age;
 
-    std::cout<< "what your name?:";
-    std::cin >> name;
+    std::cout<< "what your full name?:";    // for name
+    std::getline(std::cin >> std::ws, name);
 
 
-    std::cout<< "what your age?:";
+    std::cout<< "what your age?:";   // for age
     std::cin >> age;
 
 
-    std::cout << "hello " << name << '\n';
-    std::cout << "you are " << age << " year old";
+    std::cout << "hello" << name << '\n';
+    std::cout << "you are" << age << "year old";
 
 
 
