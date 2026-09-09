a0690d26af5c4a9615089ce9cba585ad64ceccd2
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-09-10 03:41:22 +0530

    add

--- 08_arithmetic_operators.cpp
+++ 08_arithmetic_operators.cpp
@@ -11,10 +11,24 @@ int main(){
     // students+=1;
     // students++;
 
-    students = students - 1;
-    students-=1;
-    students--;
+    // students = students - 1;
+    // students-=1;
+    // students--;
+
+    // students = students * 1;
+    // students*=1;
+
+    // students = students / 2;
+    // students/=1;
+
+    // int reminder = students % 3;        // modululas operator
+    
+    
+    students = 6 - (5 + 4) * 3 /2;       // redeclared again can see above
+
 
     std::cout << students;
+
+
     return 0;
 }
