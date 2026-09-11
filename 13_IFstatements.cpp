4f7c200410e931b5ac618afdfff0bc81f6fe87e8
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-09-11 17:11:39 +0530

    add

--- 13_IFstatements.cpp
+++ 13_IFstatements.cpp
@@ -11,8 +11,17 @@ int main(){
     std::cout << "Enter your age :";
     std::cin >> age;
 
-    if( age >= 18){
+    
+    if (age >= 100){
+        std::cout << "you are too old drink at your home not allowed to site";
+    }
+
+    else if ( age >= 18){
         std::cout<< "welcome to the site";
+    }
+
+    else if (age < 12){
+        std::cout << "you are too minor less than 12 drink milk";
     }
 
     else{
