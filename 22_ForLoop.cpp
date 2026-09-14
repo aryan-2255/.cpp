78f6fbc0aab52d024334304001f9d478de2fdd19
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-09-15 04:26:24 +0530

    add

---
+++ 22_ForLoop.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main(){
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     std::cout<< i << '\n' ;
+    // }
+
+    // std::cout<<"happy new year";
+
+// break = break out of a loop
+// continue = skip current iteration
+
+    //  for (int i = 1; i <= 10; i+=2)
+    // {
+    //     std::cout<< i << '\n' ;
+    // }
+
+    // std::cout<<"happy new year";
+
+     
+
+    // // Break
+    //  for (int i = 1; i <= 10; i++){
+    //     if (i == 3){
+    //         break;
+    //     }
+        
+    //     std::cout<< i << '\n' ;
+    // }
+    
+    // //continue
+    //  for (int i = 1; i <= 10; i++){
+    //     if (i == 3){
+    //         continue;
+    //     }
+        
+    //     std::cout<< i << '\n' ;
+    // }
+
+    return 0;
+}
