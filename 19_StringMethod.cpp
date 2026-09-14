970d739d85be53c674391bede317e8f53edd3488
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-09-15 03:31:35 +0530

    add

--- 19_StringMethod.cpp
+++ 19_StringMethod.cpp
@@ -4,16 +4,48 @@
 int main(){
     std::string name;
 
-    std::cout << "Enter your name";
+    std::cout << "Enter your name : ";
     std::getline(std::cin,name);
 
-    if (name.length() > 12){
-        std::cout <<"you name can not be greater than 12 characters";
-    }
+    // if (name.length() > 12){
+    //     std::cout <<"you name can not be greater than 12 characters";
+    // }else{
+    //     std::cout<<"welcom "<<name;
+    // }
     
+
+    // if (name.empty())
+    // {
+    //     std::cout <<"you didn't enter your name";
+    // }else{
+    //     std::cout<<"hello "<<name;
+    // }
     
+
+    // name.clear();
+    // std::cout<<"hello "<<name;
     
-    
+
+    // name.append("@gmail.com");
+    // std::cout<<"your gmail is   "<<name;
+
+
+    // std::cout << name.at(0);
+
+
+    // name.insert(0,"@");
+    // std::cout<<name;
+
+
+    // std::cout << name.find('a');
+
+
+    name.erase(0,3);
+    std::cout << name;
+
+
+
+
     
     return 0;
 }   
