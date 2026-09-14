5567a05c34b61d3825b177255a2af228aa139f7c
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-09-15 04:45:25 +0530

    add

--- 23_NestedLoop.cpp
+++ 23_NestedLoop.cpp
@@ -2,14 +2,44 @@
 
 int main(){
 
-    for (int i = 1; i <= 3; i++){
-        for (int j = 1; j <= 10; j++){
-            std::cout << j <<' ';
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "how many rows?";
+    std::cin >> rows;
+
+    std::cout << "how many columns?";
+    std::cin >> columns;
+
+    std::cout << "Enter a symbol to use?";
+    std::cin >> symbol;
+
+
+    for (int i = 1; i <= rows; i++){
+        for (int j = 1; j <= columns; j++){
+            std::cout << symbol;
         }
 
         std::cout<< '\n';
         
     }
+
+
+
+
+
+
+
+
+    // for (int i = 1; i <= 3; i++){
+    //     for (int j = 1; j <= 10; j++){
+    //         std::cout << j <<' ';
+    //     }
+
+    //     std::cout<< '\n';
+        
+    // }
     
 
 
